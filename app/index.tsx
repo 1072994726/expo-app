ed5c0e212ed5a4799c0f371c651629fb7b759c1d
xuangang <1072994726@qq.com>
2024-10-22 23:59:31 +0800

    feat: 学习导航内容

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>Home Screen</Text>
+      <Link href={'/about1'} style={styles.button}>Go To About Page</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: '#fff'
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+})
